f5048c62d7f27a9b92c52408155b5eae7466f2fb
juan-carlos-mamani-gutierrez <jiseroca65528@gmail.com>
2023-12-26 05:18:26 +0100

    conexion a la base de datos

---
+++ config/conexion.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+
+class Conectar
+{
+  protected $dbh;
+  /* TODO funcion para realizar la conexion */
+  protected function Conexion()
+  {
+    try {
+
+      $conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=cotizador", "root", "");
+      return $conectar;
+    } catch (Exception $e) {
+
+      print "!Error DB : " . $e->getMessage() . "<br/>";
+      die();
+    }
+  }
+  /* TODO funcion para configurar la codificacion de caracteres */
+  public function set_names()
+  {
+
+    return $this->dbh->query("SET NAMES 'utf8'");
+  }
+  /* TODO funcion para obtener la ruta de aplicacion */
+  public  static function ruta()
+  {
+
+    return "http://localhost/cotizador";
+  }
+}
